b792f7cf3b5fa3ec9fbc2b973eb361eff87cbc3c
Atmanss <otmanssbariia@gmail.com>
2023-10-15 09:47:10 +0100

    Printf

--- precision.c
+++ precision.c
@@ -12,9 +12,12 @@ int get_precision(const char *format, int *i, va_list list)
 {
 	int curr_i = *i + 1;
 	int precision = -1;
+
 	if (format[curr_i] != '.')
 		return (precision);
+
 	precision = 0;
+
 	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 	{
 		if (is_digit(format[curr_i]))
@@ -25,16 +28,14 @@ int get_precision(const char *format, int *i, va_list list)
 		else if (format[curr_i] == '*')
 		{
 			curr_i++;
-			int arg_precision = va_arg(list, int);
-			if (arg_precision >= 0)
-			{
-				precision = arg_precision;\
-			}
+			precision = va_arg(list, int);
 			break;
 		}
 		else
 			break;
 	}
+
 	*i = curr_i - 1;
+
 	return (precision);
 }
